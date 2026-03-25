9462c7c702774c05a6af0bff0279d1d8f1bfbf8a
serguney-gumus <gumus.serguney@gmail.com>
2026-03-25 14:21:04 +0100

    menu implemented

--- main.c
+++ main.c
@@ -1,10 +1,57 @@
+// This script drives program
+
+// Call the libraries
 #include<stdlib.h>
 #include<stdio.h>
 
-// This script drives program
+// Function gets integer input from user
+int getOperation(int);
+// Function operates menu
+void menu(void);
+
+// Main function
 
 int main ()
 {
-    printf("Hello world");
+    menu();
     return 0;
+};
+
+void menu(void)
+{
+    int operation = 0;
+    
+    // Stay in the looop until program is terminated
+    while (operation != 3)
+    {
+        /*Menu Operations*/
+        printf("Encryption!\n");
+        printf("(1) Change the secret code\n");
+        printf("(2) Encrypt text\n");
+        printf("(3) Quit\n");
+        operation = getOperation(operation);
+
+    };
+}
+
+int getOperation(int opSelect)
+{
+    // Caharacter array for the input
+    char input[100];
+    
+    // Get input
+    printf("Please enter your operation(e.g. 1, 2):");
+    fgets(input, sizeof(input), stdin);
+    sscanf(input, "%d", &opSelect);
+    
+    // Check if the entered input valid 
+    if (opSelect == 1 || opSelect == 2 || opSelect == 3)
+    {
+        return opSelect;   
+    }
+
+    // Print error msg
+    printf("Invalid input entered!\n");
+    opSelect = 0;
+    return opSelect;
 };
